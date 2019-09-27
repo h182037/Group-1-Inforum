f4a001d8bd815cd55d599837b081904b7ec3254b
Håkon Robbestad Gylterud <hakon.gylterud@uib.no>
2019-09-27 17:16:06 +0200

    Added UUID generation to Stored objects.

--- src/main/java/inf226/inforum/storage/Storage.java
+++ src/main/java/inf226/inforum/storage/Storage.java
@@ -6,7 +6,7 @@ import inf226.inforum.storage.*;
 public interface Storage<T,Q,E extends Exception> {
 
    public Stored<T> save(T value) throws E;
-   public Stored<T> update(Stored<T> object) throws UpdatedException,DeletedException,E;
+   public Stored<T> update(Stored<T> object, T new_object) throws UpdatedException,DeletedException,E;
    public void delete(Stored<T> object) throws UpdatedException,DeletedException,E;
 
    public ImmutableList< Stored<T> > lookup(Q query) throws E;
--- src/main/java/inf226/inforum/storage/Stored.java
+++ src/main/java/inf226/inforum/storage/Stored.java
@@ -1,11 +1,40 @@
 package inf226.inforum.storage;
 
-public class Stored<T> {
+import java.util.UUID;
 
+public class Stored<T> {
   public final T value;
+  public final UUID identity;
+  public final UUID version;
 
   public Stored(T value) {
     this.value = value;
+    this.identity = UUID.randomUUID();
+    this.version = UUID.randomUUID();
+  }
+
+  public Stored(T value, UUID identity, UUID version) {
+    this.value = value;
+    this.identity = identity;
+    this.version = version;
+  }
+
+  @Override
+  public boolean equals(Object other) {
+    if (other == null)
+        return false;
+    if (getClass() != other.getClass())
+        return false;
+    @SuppressWarnings("unchecked")
+    final Stored<T> stored_other = (Stored<T>) other;
+    return this.identity.equals(stored_other.identity)
+        && this.version.equals(stored_other.version)
+        && this.value.equals(stored_other.value);
+
+  }
+
+  Stored<T> newVersion() {
+     return new Stored<T>(value , identity, UUID.randomUUID());
   }
 }
 
