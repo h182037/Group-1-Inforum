0a79966406fb272150eb9f159f17554c82ef91b8
Håkon Robbestad Gylterud <hakon@gylterud.net>
2019-10-13 21:16:29 +0200

    Implementing saving messages to DB.

--- src/main/java/inf226/inforum/storage/MessageStorage.java
+++ src/main/java/inf226/inforum/storage/MessageStorage.java
@@ -10,6 +10,15 @@ import java.io.IOException;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+import java.util.UUID;
+import java.time.Instant;
+
+/**
+ * TODO: Secure the following for SQL injection vulnerabilities.
+ */
 
 public class MessageStorage implements Storage<Message,String,SQLException>, Closeable {
    final Connection connection;
@@ -28,15 +37,54 @@ public class MessageStorage implements Storage<Message,String,SQLException>, Clo
       }
    }
 
+   public void initialise() throws SQLException {
+       connection.createStatement()
+                 .executeUpdate("CREATE TABLE IF NOT EXISTS Message (id TEXT, version TEXT, sender TEXT, message TEXT, date TEXT) PRIMARY KEY (id)");
+   }
 
-   @Override
-   public Stored<Message> save(Message value) throws SQLException {
-     return null;
+   private Stored<Message> getCurrent(UUID id) throws DeletedException,SQLException {
+      final String sql = "SELECT (version,sender,message,date) FROM Message WHERE id='" + id.toString() + "'";
+      final Statement statement = connection.createStatement();
+      final ResultSet rs = statement.executeQuery(sql);
+
+      if(rs.next()) {
+          final UUID version = UUID.fromString(rs.getString("version"));
+          final String sender = rs.getString("sender");
+          final String message = rs.getString("message");
+          final Instant date = Instant.parse(rs.getString("date"));
+          return (new Stored<Message>(new Message(sender,message,date),id,version));
+      } else {
+          throw new DeletedException();
+      }
    }
 
    @Override
-   public Stored<Message> update(Stored<Message> object, Message new_object) throws UpdatedException,DeletedException,SQLException {
-     return null;
+   public Stored<Message> save(Message message) throws SQLException {
+     final Stored<Message> stored = new Stored<Message>(message);
+     String sql =  "INSERT INTO Message VALUES(" + stored.identity + ","
+                                                 + stored.version  + ","
+                                                 + message.sender  + ","
+                                                 + message.message + ","
+                                                 + message.date.toString() + ")";
+     connection.createStatement().executeUpdate(sql);
+     return stored;
+   }
+
+   @Override
+   public synchronized Stored<Message> update(Stored<Message> message, Message new_message) throws UpdatedException,DeletedException,SQLException {
+     final Stored<Message> current = getCurrent(message.identity);
+     final Stored<Message> updated = current.newVersion(new_message);
+     if(current.version == message.version) {
+        String sql =  "REPLACE INTO Message VALUES(" + updated.identity + ","
+                                                     + updated.version  + ","
+                                                     + new_message.sender  + ","
+                                                     + new_message.message + ","
+                                                     + new_message.date.toString() + ")";
+        connection.createStatement().executeUpdate(sql);
+     } else {
+        throw new UpdatedException(current);
+     }
+     return updated;
    }
 
    @Override
